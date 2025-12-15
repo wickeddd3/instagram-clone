2aee74b5ee45f70461eb9c4ca08eca215a7c3776
wickeddd3 <pablophilipandrew@gmail.com>
2025-12-15 23:14:59 +0800

    feat: wrap main App component in ApolloProvider

--- instagram-frontend/src/main.tsx
+++ instagram-frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
 import { App } from "./App.tsx";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: import.meta.env.VITE_API_URL }),
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
