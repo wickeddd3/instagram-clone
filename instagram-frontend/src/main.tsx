9651b0e9d4501afba885e62cf4d1b9bb57d29978
wickeddd3 <pablophilipandrew@gmail.com>
2025-12-31 19:00:06 +0800

    feat: add apollo lib and add handling of auth access_token sending on every request

--- instagram-frontend/src/main.tsx
+++ instagram-frontend/src/main.tsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
 import { App } from "./App.tsx";
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: import.meta.env.VITE_API_URL }),
-  cache: new InMemoryCache(),
-});
+import { client } from "./lib/apollo.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
