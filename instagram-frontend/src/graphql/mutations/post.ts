c563b349745234527350c3142af035a6c8d62101
wickeddd3 <pablophilipandrew@gmail.com>
2025-12-31 19:08:02 +0800

    feat: use updated post property

--- instagram-frontend/src/graphql/mutations/post.ts
+++ instagram-frontend/src/graphql/mutations/post.ts
@@ -3,7 +3,7 @@ import { gql } from "@apollo/client";
 export const CREATE_POST = gql`
   mutation CreatePost($imageUrl: String!, $caption: String) {
     createPost(imageUrl: $imageUrl, caption: $caption) {
-      image_url
+      imageUrl
       caption
     }
   }
