9e914fb87a3e8c2757252ac5fe70d3b2e89cf0be
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-02 22:23:17 +0800

    feat: pass email as param on createProfile

--- instagram-frontend/src/graphql/mutations/profile.ts
+++ instagram-frontend/src/graphql/mutations/profile.ts
@@ -1,10 +1,11 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_PROFILE_MUTATION = gql`
-  mutation CreateProfile($id: ID!, $username: String!) {
-    createProfile(id: $id, username: $username, avatarUrl: null) {
+  mutation CreateProfile($id: ID!, $username: String!, $email: String!) {
+    createProfile(id: $id, username: $username, email: $email) {
       id
       username
+      email
     }
   }
 `;
