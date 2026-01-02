534160266e461af5f1217aaceb87c49d276dc331
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-02 22:22:07 +0800

    feat: add getProfile graphql query

---
+++ instagram-frontend/src/graphql/queries/profile.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const GET_PROFILE = gql`
+  query GetProfile($id: ID!) {
+    getProfile(id: $id) {
+      id
+      username
+      displayName
+      avatarUrl
+      bio
+      website
+    }
+  }
+`;
