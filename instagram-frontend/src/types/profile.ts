0d6216c521d9cb1e72db99f942c876b86dbac7b0
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-03 16:23:20 +0800

    feat: add profile type

---
+++ instagram-frontend/src/types/profile.ts
@@ -0,0 +1,10 @@
+export interface ProfileData {
+  getProfile: {
+    id: string;
+    displayName: string;
+    username: string;
+    avatarUrl: string;
+    bio: string;
+    website: string;
+  };
+}
