47de56d25a96aa0a3e228617368f1a223a9af6aa
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-04 23:20:54 +0800

    feat: add ADD_COMMENT graphql mutation

---
+++ instagram-frontend/src/graphql/mutations/comment.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const ADD_COMMENT = gql`
+  mutation AddComment($postId: ID!, $text: String!, $parentId: ID) {
+    addComment(postId: $postId, text: $text, parentId: $parentId) {
+      id
+      text
+      createdAt
+      author {
+        id
+        username
+        avatarUrl
+      }
+    }
+  }
+`;
