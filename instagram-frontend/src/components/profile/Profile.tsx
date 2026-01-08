78bdaa8023133e62711a144e396588b964b95da9
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-08 14:07:20 +0800

    feat: add Profile component using compound pattern

---
+++ instagram-frontend/src/components/profile/Profile.tsx
@@ -0,0 +1,140 @@
+import { Bookmark, Cog, Grid, SquareUser } from "lucide-react";
+import { useState } from "react";
+import { Posts } from "./Posts";
+import { SavedPosts } from "./SavedPosts";
+import { TaggedPosts } from "./TaggedPosts";
+
+export const Profile = ({ children }: { children: React.ReactNode }) => {
+  return <div className="max-w-3xl w-full mx-auto px-4 pt-8">{children}</div>;
+};
+
+export const Avatar = ({ avatarUrl }: { avatarUrl: string }) => {
+  return (
+    <div className="w-20 h-20 md:w-30 md:h-30 rounded-full bg-gray-800 overflow-hidden">
+      <img
+        src={avatarUrl}
+        alt="Avatar"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+};
+
+export const SettingsButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-1 hover:text-gray-400 transition cursor-pointer"
+    >
+      <Cog size={24} />
+    </button>
+  );
+};
+
+export const ActionButton = ({
+  label,
+  onClick,
+}: {
+  label: string;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      className="w-full bg-gray-800 hover:bg-gray-700 text-white p-1.5 md:p-3 rounded-lg md:rounded-xl text-sm font-semibold transition cursor-pointer"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export const Stats = ({
+  postsCount,
+  followersCount,
+  followingCount,
+}: {
+  postsCount: number;
+  followersCount: number;
+  followingCount: number;
+}) => {
+  return (
+    <div className="hidden md:flex gap-10">
+      <span className="text-sm">
+        <strong>{postsCount}</strong> posts
+      </span>
+      <span className="text-sm">
+        <strong>{followersCount}</strong> followers
+      </span>
+      <span className="text-sm">
+        <strong>{followingCount}</strong> following
+      </span>
+    </div>
+  );
+};
+
+export const DisplayName = ({ name }: { name: string }) => {
+  return <h1 className="text-sm font-normal">{name}</h1>;
+};
+
+export const Username = ({ name }: { name: string }) => {
+  return <h2 className="text-2xl font-bold">{name}</h2>;
+};
+
+export const Bio = ({ text }: { text: string }) => {
+  return <p className="text-sm">{text}</p>;
+};
+
+export const Content = ({ profileId }: { profileId: string }) => {
+  const [activeTab, setActiveTab] = useState<"POSTS" | "SAVED" | "TAGGED">(
+    "POSTS"
+  );
+
+  return (
+    <div className="flex flex-col">
+      <div className="grid grid-cols-3 place-items-center">
+        <button
+          onClick={() => setActiveTab("POSTS")}
+          className={`px-6 py-2 cursor-pointer transition ${
+            activeTab === "POSTS"
+              ? "text-white border-white border-b-2"
+              : "text-gray-400"
+          }`}
+        >
+          <Grid size={24} />
+        </button>
+        <button
+          onClick={() => setActiveTab("SAVED")}
+          className={`px-6 py-2 cursor-pointer transition ${
+            activeTab === "SAVED"
+              ? "text-white border-white border-b-2"
+              : "text-gray-400"
+          }`}
+        >
+          <Bookmark size={24} />
+        </button>
+        <button
+          onClick={() => setActiveTab("TAGGED")}
+          className={`px-6 py-2 cursor-pointer transition ${
+            activeTab === "TAGGED"
+              ? "text-white border-white border-b-2"
+              : "text-gray-400"
+          }`}
+        >
+          <SquareUser size={24} />
+        </button>
+      </div>
+      {activeTab === "POSTS" && <Posts profileId={profileId} />}
+      {activeTab === "SAVED" && <SavedPosts />}
+      {activeTab === "TAGGED" && <TaggedPosts />}
+    </div>
+  );
+};
+
+Profile.Avatar = Avatar;
+Profile.SettingsButton = SettingsButton;
+Profile.ActionButton = ActionButton;
+Profile.Stats = Stats;
+Profile.DisplayName = DisplayName;
+Profile.Username = Username;
+Profile.Bio = Bio;
+Profile.Content = Content;
