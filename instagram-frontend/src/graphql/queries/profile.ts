fb1dbc40696c87fffa8122385ecd28dc8a30d05d
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-08 22:13:01 +0800

    feat: add GET_PROFILE by username mutation

--- instagram-frontend/src/graphql/queries/profile.ts
+++ instagram-frontend/src/graphql/queries/profile.ts
@@ -1,8 +1,25 @@
 import { gql } from "@apollo/client";
 
 export const GET_PROFILE = gql`
-  query GetProfile($id: ID!) {
-    getProfile(id: $id) {
+  query GetProfile($username: String!) {
+    getProfile(username: $username) {
+      id
+      username
+      displayName
+      avatarUrl
+      bio
+      website
+      postsCount
+      followersCount
+      followingCount
+      isFollowing
+    }
+  }
+`;
+
+export const GET_PROFILE_BY_ID = gql`
+  query GetProfileById($id: ID!) {
+    getProfileById(id: $id) {
       id
       username
       displayName
