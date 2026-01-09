7f92e9ec9a613fc7d3f440c7739ee78c88a95b53
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-10 02:34:10 +0800

    feat: handle saved posts listing

--- instagram-frontend/src/components/profile/SavedPosts.tsx
+++ instagram-frontend/src/components/profile/SavedPosts.tsx
@@ -1,3 +1,30 @@
+import { useQuery } from "@apollo/client/react";
+import type { SavedPostsData } from "../../types/post";
+import { GET_SAVED_POSTS } from "../../graphql/queries/post";
+import { PostItem } from "./PostItem";
+
 export const SavedPosts = () => {
-  return <span className="text-center py-8">saved posts</span>;
+  const { loading, error, data } = useQuery<SavedPostsData>(GET_SAVED_POSTS);
+
+  return (
+    <>
+      {loading && (
+        <div className="flex w-full justify-center pt-20 text-white">
+          Loading posts...
+        </div>
+      )}
+
+      {error && (
+        <div className="flex w-full justify-center pt-20 text-red-500">
+          Error: {error.message}
+        </div>
+      )}
+
+      <div className="grid grid-cols-3 gap-0.5">
+        {data?.getSavedPosts.map((post) => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
