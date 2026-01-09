d9209d749583e1d7a69787924ed3eca081a816de
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-09 20:55:48 +0800

    feat: add follow SearchField

---
+++ instagram-frontend/src/components/follow/SearchField.tsx
@@ -0,0 +1,10 @@
+export const SearchField = () => {
+  return (
+    <input
+      name="search"
+      type="text"
+      placeholder="Search"
+      className="w-full px-3 py-2 bg-neutral-700 rounded-xl outline-none text-sm"
+    />
+  );
+};
