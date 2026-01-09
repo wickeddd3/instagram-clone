0ae5bb9e32ead0e8567a5345840bf70fe5b00a52
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-09 20:53:42 +0800

    feat: add FollowerList

---
+++ instagram-frontend/src/components/follow/FollowerList.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@apollo/client/react";
+import { SearchField } from "./SearchField";
+import { FollowList } from "./FollowList";
+import { GET_FOLLOWERS } from "../../graphql/queries/profile";
+import { Loader2 } from "lucide-react";
+import type { FollowersData } from "../../types/profile";
+
+export const FollowerList = ({ username }: { username: string }) => {
+  const { data, loading } = useQuery<FollowersData>(GET_FOLLOWERS, {
+    variables: { username },
+    skip: !username,
+  });
+
+  if (loading)
+    return (
+      <div className="flex justify-center pt-20">
+        <Loader2 className="animate-spin" />
+      </div>
+    );
+
+  return (
+    <div className="w-full h-full flex flex-col gap-3 py-2 px-4">
+      <SearchField />
+      <FollowList profiles={data?.getFollowers || []} />
+    </div>
+  );
+};
