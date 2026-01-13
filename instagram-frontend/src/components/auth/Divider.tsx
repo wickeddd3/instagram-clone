0a3a69f427d9ad75224852b1eb67fe424cbff0cb
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-13 23:08:20 +0800

    feat: add auth Divider

---
+++ instagram-frontend/src/components/auth/Divider.tsx
@@ -0,0 +1,9 @@
+export const Divider = () => {
+  return (
+    <div className="w-full flex justify-center items-center gap-4">
+      <div className="flex-1 border-t border-gray-800"></div>
+      <span className="w-fit text-sm font-bold text-gray-400">OR</span>
+      <div className="flex-1 border-t border-gray-800"></div>
+    </div>
+  );
+};
