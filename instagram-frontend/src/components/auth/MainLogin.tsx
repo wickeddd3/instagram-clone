533747e321f4acda5045416014d9c105e8845606
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 00:49:24 +0800

    feat: handle login error

--- instagram-frontend/src/components/auth/MainLogin.tsx
+++ instagram-frontend/src/components/auth/MainLogin.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Divider } from "./Divider";
+import { ErrorMessage } from "./ErrorMessage";
 import { Footer } from "./Footer";
 import { IgLabel } from "./Iglabel";
 import { LoginForm } from "./LoginForm";
@@ -7,6 +9,8 @@ import { LoginWithFbLink } from "./links/LoginWithFbLink";
 import { NoAccountLink } from "./links/NoAccountLink";
 
 export const MainLogin = () => {
+  const [hasError, setHasError] = useState(false);
+
   return (
     <div className="w-full h-full min-h-screen flex flex-col justify-between md:justify-center items-center gap-12 p-4 bg-[#0d1015] text-white">
       <div className="flex-1 w-full h-full flex justify-center items-center gap-16 p-4">
@@ -16,9 +20,12 @@ export const MainLogin = () => {
         <div className="w-xs flex flex-col justify-center items-center gap-12">
           <IgLabel />
           <div className="w-full flex flex-col justify-center items-center gap-4">
-            <LoginForm />
+            <LoginForm onError={setHasError} />
             <Divider />
             <LoginWithFbLink />
+            {hasError && (
+              <ErrorMessage message="Sorry, your password was incorrect. Please double-check your password." />
+            )}
             <ForgotPasswordLink />
           </div>
           <NoAccountLink />
