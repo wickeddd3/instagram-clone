70ae6bd01746e2955ddf15a01678af39669358a3
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 00:47:41 +0800

    feat: TextField handle hook form props

--- instagram-frontend/src/components/auth/TextField.tsx
+++ instagram-frontend/src/components/auth/TextField.tsx
@@ -1,17 +1,27 @@
-interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+import { forwardRef } from "react";
+
+interface TextFieldProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  type: "text" | "password";
+  error?: string;
   label: string;
 }
 
-export const TextField = ({ label, ...props }: TextFieldProps) => {
-  return (
-    <div className="relative w-full mb-2">
-      <input
-        {...props}
-        className="peer w-full bg-gray-800 border border-gray-600 rounded-sm px-2 pt-4 pb-1 text-xs focus:outline-none focus:border-gray-500 placeholder-transparent transition-all"
-        placeholder={label}
-      />
-      <label
-        className="absolute left-2 text-gray-500 text-xs transition-all pointer-events-none
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ type = "text", label, error, ...props }, ref) => {
+    return (
+      <div className="relative w-full mb-2">
+        <input
+          {...props}
+          ref={ref}
+          type={type}
+          className={`peer w-full bg-gray-800 border ${
+            error ? "border-red-500" : "border-gray-700"
+          } rounded-sm px-2 pt-4 pb-1 text-xs text-white focus:outline-none focus:border-gray-500 placeholder-transparent transition-all`}
+          placeholder={label}
+        />
+        <label
+          className="absolute left-2 text-gray-500 text-xs transition-all pointer-events-none
         top-3 
         peer-placeholder-shown:top-2.5 
         peer-placeholder-shown:text-sm 
@@ -19,9 +29,11 @@ export const TextField = ({ label, ...props }: TextFieldProps) => {
         peer-focus:text-[10px] 
         peer-[:not(:placeholder-shown)]:top-1 
         peer-[:not(:placeholder-shown)]:text-[10px]"
-      >
-        {label}
-      </label>
-    </div>
-  );
-};
+        >
+          {label}
+        </label>
+        {error && <p className="text-[10px] text-red-500 mt-1">{error}</p>}
+      </div>
+    );
+  }
+);
