1f86a78f70035bef6910491bbd32033dc3d44a71
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 00:49:12 +0800

    feat: handle login

--- instagram-frontend/src/components/auth/LoginForm.tsx
+++ instagram-frontend/src/components/auth/LoginForm.tsx
@@ -1,14 +1,58 @@
 import { SubmitButton } from "./SubmitButton";
 import { TextField } from "./TextField";
+import { useAuth } from "../../contexts/AuthContext";
+import { useForm } from "react-hook-form";
+import { LoginFormSchema, type LoginFormType } from "../../validation/login";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Loader } from "lucide-react";
 
-export const LoginForm = () => {
+interface LoginFormProps {
+  onError: (value: boolean) => void;
+}
+
+export const LoginForm = ({ onError }: LoginFormProps) => {
+  const { signIn } = useAuth();
+
+  const { register, handleSubmit } = useForm<LoginFormType>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = handleSubmit(async (data: LoginFormType) => {
+    setLoading(true);
+    try {
+      const { error } = await signIn(data.email, data.password);
+      setLoading(false);
+      onError(false);
+      if (error) {
+        onError(true);
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  });
+
   return (
-    <div className="w-full h-full flex flex-col gap-2">
+    <form onSubmit={handleLogin} className="w-full h-full flex flex-col gap-2">
       <div className="flex flex-col">
-        <TextField label="Phone number, username, or email" type="text" />
-        <TextField label="Password" type="password" />
+        <TextField
+          label="Phone number, username, or email"
+          type="text"
+          {...register("email")}
+        />
+        <TextField label="Password" type="password" {...register("password")} />
       </div>
-      <SubmitButton label="Log in" onClick={() => {}} />
-    </div>
+      <SubmitButton disabled={loading}>
+        {loading ? <Loader className="animate-spin" size={20} /> : "Log in"}
+      </SubmitButton>
+    </form>
   );
 };
