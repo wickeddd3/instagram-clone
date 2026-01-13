e68792c439eac7a8e5cb7107bc0458758d509542
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 00:47:58 +0800

    feat: add additional props

--- instagram-frontend/src/components/auth/SubmitButton.tsx
+++ instagram-frontend/src/components/auth/SubmitButton.tsx
@@ -1,19 +1,28 @@
+import type { ReactNode } from "react";
+
 interface SubmitButtonProps {
-  label: string;
+  label?: string;
+  disabled?: boolean;
   onClick?: () => void;
   className?: string;
+  children?: ReactNode;
 }
 
 export const SubmitButton = ({
   label,
+  disabled = false,
   onClick,
   className,
+  children,
 }: SubmitButtonProps) => {
   return (
     <button
+      type="submit"
       onClick={onClick}
-      className={`bg-indigo-700 hover:bg-indigo-600 text-white text-sm font-semibold rounded-md p-1.5 w-full ${className}`}
+      disabled={disabled}
+      className={`bg-indigo-700 hover:bg-indigo-600 text-white text-sm font-semibold flex justify-center items-center rounded-md p-1.5 w-full cursor-pointer ${className}`}
     >
+      {children}
       {label}
     </button>
   );
