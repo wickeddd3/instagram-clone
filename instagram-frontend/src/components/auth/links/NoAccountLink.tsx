3b88c226300861db3a03161a70d97751c4f4ca1e
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-13 23:08:55 +0800

    feat: add auth NoAccountLink

---
+++ instagram-frontend/src/components/auth/links/NoAccountLink.tsx
@@ -0,0 +1,10 @@
+export const NoAccountLink = () => {
+  return (
+    <div className="flex justify-center items-center gap-2 text-sm">
+      <span className="text-gray-200">Don't have an account?</span>
+      <span className="text-indigo-400 font-semibold cursor-pointer">
+        Sign up
+      </span>
+    </div>
+  );
+};
