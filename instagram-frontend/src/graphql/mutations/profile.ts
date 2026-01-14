44a207ab72b7570452befe6b994ef40d4110959c
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 13:58:07 +0800

    feat: add displayName when creating profile

--- instagram-frontend/src/graphql/mutations/profile.ts
+++ instagram-frontend/src/graphql/mutations/profile.ts
@@ -1,11 +1,22 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_PROFILE_MUTATION = gql`
-  mutation CreateProfile($id: ID!, $username: String!, $email: String!) {
-    createProfile(id: $id, username: $username, email: $email) {
+  mutation CreateProfile(
+    $id: ID!
+    $username: String!
+    $email: String!
+    $displayName: String!
+  ) {
+    createProfile(
+      id: $id
+      username: $username
+      email: $email
+      displayName: $displayName
+    ) {
       id
       username
       email
+      displayName
     }
   }
 `;
