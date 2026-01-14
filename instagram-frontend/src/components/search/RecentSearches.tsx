86564cf384090e3a8d017b4589389a4f82246851
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-15 01:36:32 +0800

    feat: add search RecentSearches

---
+++ instagram-frontend/src/components/search/RecentSearches.tsx
@@ -0,0 +1,47 @@
+import { useQuery } from "@apollo/client/react";
+import { GET_RECENT_SEARCHES } from "../../graphql/queries/profile";
+import { SearchResultItem } from "./SearchResultItem";
+import type { RecentSearchesData } from "../../types/profile";
+import { useEffect } from "react";
+
+interface RecentSearchesProps {
+  query: string;
+  onClose: () => void;
+}
+
+export const RecentSearches = ({ query, onClose }: RecentSearchesProps) => {
+  const {
+    data,
+    loading,
+    refetch: refetchHistory,
+  } = useQuery<RecentSearchesData>(GET_RECENT_SEARCHES);
+
+  useEffect(() => {
+    refetchHistory();
+  }, [query]);
+
+  if (loading) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <span className="text-gray-500 text-sm">Fetching...</span>
+      </div>
+    );
+  }
+
+  if (!loading && !data?.getRecentSearches.length) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <span className="text-gray-500 text-sm">No recent searches.</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-full flex flex-col gap-1">
+      <div className="text-sm font-bold mt-2 px-2 pb-2">Recent</div>
+      {data?.getRecentSearches.map((user) => (
+        <SearchResultItem user={user} onClick={onClose} />
+      ))}
+    </div>
+  );
+};
