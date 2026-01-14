dbb1e5d69309fc388157b96550fac98af4661293
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-15 02:03:41 +0800

    feat: handle remove recent search

--- instagram-frontend/src/components/search/SearchResultItem.tsx
+++ instagram-frontend/src/components/search/SearchResultItem.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import type { ProfileData } from "../../types/profile";
-import { ADD_RECENT_SEARCH } from "../../graphql/mutations/profile";
+import {
+  ADD_RECENT_SEARCH,
+  REMOVE_RECENT_SEARCH,
+} from "../../graphql/mutations/profile";
 import { useMutation } from "@apollo/client/react";
+import { X } from "lucide-react";
 
 interface SearchResultItemProps {
   user: ProfileData;
@@ -10,10 +14,20 @@ interface SearchResultItemProps {
 
 export const SearchResultItem = ({ user, onClick }: SearchResultItemProps) => {
   const [addRecentSearch] = useMutation(ADD_RECENT_SEARCH);
+  const [removeRecentSearch] = useMutation(REMOVE_RECENT_SEARCH);
 
   const handleProfileClick = (targetId: string) => {
     addRecentSearch({ variables: { targetId } });
     onClick();
+  };
+
+  const handleRemoveRecentSearch = (
+    event: React.MouseEvent,
+    targetId: string
+  ) => {
+    event.preventDefault(); // Stop Link navigation
+    event.stopPropagation(); // Stop event from reaching the Link
+    removeRecentSearch({ variables: { targetId } });
   };
 
   return (
@@ -21,16 +35,24 @@ export const SearchResultItem = ({ user, onClick }: SearchResultItemProps) => {
       key={user.id}
       to={`/${user.username}`}
       onClick={() => handleProfileClick(user.id)}
-      className="flex items-center gap-3 p-2 hover:bg-gray-800 rounded-lg transition"
+      className="flex items-center justify-between gap-2 p-2 hover:bg-gray-800 rounded-lg transition"
     >
-      <img
-        src={user.avatarUrl || "/default-avatar.png"}
-        className="w-11 h-11 rounded-full object-cover"
-      />
-      <div className="flex flex-col">
-        <span className="text-sm font-bold">{user.username}</span>
-        <span className="text-sm text-gray-400">{user.displayName}</span>
+      <div className="flex items-center gap-3">
+        <img
+          src={user.avatarUrl || "/default-avatar.png"}
+          className="w-11 h-11 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <span className="text-sm font-bold">{user.username}</span>
+          <span className="text-sm text-gray-400">{user.displayName}</span>
+        </div>
       </div>
+      <button
+        onClick={(event) => handleRemoveRecentSearch(event, user.id)}
+        className="cursor-pointer"
+      >
+        <X size={24} className="text-gray-400" />
+      </button>
     </Link>
   );
 };
