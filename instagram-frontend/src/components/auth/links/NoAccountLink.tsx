0c3c3c7d87a0502cb24c04e2b95ce942497bb1ea
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-14 13:45:21 +0800

    feat: add link route

--- instagram-frontend/src/components/auth/links/NoAccountLink.tsx
+++ instagram-frontend/src/components/auth/links/NoAccountLink.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const NoAccountLink = () => {
   return (
-    <div className="flex justify-center items-center gap-2 text-sm">
+    <Link
+      to="/accounts/signup"
+      className="flex justify-center items-center gap-2 text-sm"
+    >
       <span className="text-gray-200">Don't have an account?</span>
       <span className="text-indigo-400 font-semibold cursor-pointer">
         Sign up
       </span>
-    </div>
+    </Link>
   );
 };
