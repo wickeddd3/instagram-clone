6a46749580b03c51a56a6574bd8fb58130e0846c
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-15 17:57:15 +0800

    feat: add GET_EXPLORE_POSTS graphql query

--- instagram-frontend/src/graphql/queries/post.ts
+++ instagram-frontend/src/graphql/queries/post.ts
@@ -19,6 +19,25 @@ export const GET_FEED = gql`
   }
 `;
 
+export const GET_EXPLORE_POSTS = gql`
+  query GetExplorePosts($limit: Int, $offset: Int) {
+    getExplorePosts(limit: $limit, offset: $offset) {
+      id
+      imageUrl
+      caption
+      createdAt
+      author {
+        username
+        avatarUrl
+      }
+      likesCount
+      commentsCount
+      isLiked
+      isSaved
+    }
+  }
+`;
+
 export const GET_PROFILE_POSTS = gql`
   query GetProfilePosts($profileId: ID!, $limit: Int, $offset: Int) {
     getProfilePosts(profileId: $profileId, limit: $limit, offset: $offset) {
