baacff537cd88abfa6c90d0e387c5cb54411cc84
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-16 03:16:46 +0800

    feat: use SuggestionItem

--- instagram-frontend/src/components/layouts/SuggestionsSidebar.tsx
+++ instagram-frontend/src/components/layouts/SuggestionsSidebar.tsx
@@ -1,8 +1,8 @@
-import { useQuery, useMutation } from "@apollo/client/react";
+import { useQuery } from "@apollo/client/react";
 import { GET_SUGGESTED_PROFILES } from "../../graphql/queries/profile";
-import { TOGGLE_FOLLOW } from "../../graphql/mutations/profile";
 import { Link } from "react-router-dom";
 import type { SuggestedProfilesData } from "../../types/profile";
+import { SuggestionItem } from "./SuggestionItem";
 
 export const SuggestionsSidebar = () => {
   const { data, loading } = useQuery<SuggestedProfilesData>(
@@ -11,11 +11,6 @@ export const SuggestionsSidebar = () => {
       variables: { limit: 5 },
     }
   );
-  const [toggleFollow] = useMutation(TOGGLE_FOLLOW);
-
-  const handleToggleFollow = (username: string) => {
-    toggleFollow({ variables: { username } });
-  };
 
   if (loading)
     return <div className="animate-pulse w-full h-40 bg-gray-900 rounded-lg" />;
@@ -37,31 +32,8 @@ export const SuggestionsSidebar = () => {
 
       {/* Suggested Users List */}
       <div className="flex flex-col gap-3">
-        {data?.getSuggestedProfiles.map((user: any) => (
-          <div key={user.id} className="flex items-center justify-between">
-            <Link to={`/${user.username}`} className="flex items-center gap-3">
-              <img
-                src={user.avatarUrl || "/default.png"}
-                className="w-8 h-8 rounded-full object-cover"
-              />
-              <div className="flex flex-col leading-tight">
-                <span className="text-sm font-bold text-white">
-                  {user.username}
-                </span>
-                <span className="text-[12px] text-gray-500">
-                  {user.followersCount > 0
-                    ? `Followed by ${user.mutualFriend}`
-                    : "New to Instagram"}
-                </span>
-              </div>
-            </Link>
-            <button
-              onClick={() => handleToggleFollow(user.username)}
-              className="text-xs font-bold text-indigo-400 hover:text-indigo-300 cursor-pointer"
-            >
-              Follow
-            </button>
-          </div>
+        {data?.getSuggestedProfiles.map((user) => (
+          <SuggestionItem user={user} />
         ))}
       </div>
     </div>
