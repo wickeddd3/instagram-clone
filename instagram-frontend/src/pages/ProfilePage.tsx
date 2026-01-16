9c723cd4323c5628a238261c1857f36e60f4438c
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-16 20:24:09 +0800

    feat: use ProfileHeaderSkeleton

--- instagram-frontend/src/pages/ProfilePage.tsx
+++ instagram-frontend/src/pages/ProfilePage.tsx
@@ -1,4 +1,3 @@
-import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import { useMutation, useQuery } from "@apollo/client/react";
 import { SettingsModal } from "../components/modals/SettingsModal";
@@ -7,6 +6,7 @@ import { Profile } from "../components/profile/Profile";
 import type { ProfileDataByUsername } from "../types/profile";
 import { GET_PROFILE } from "../graphql/queries/profile";
 import { TOGGLE_FOLLOW } from "../graphql/mutations/profile";
+import { ProfileHeaderSkeleton } from "../components/loaders/ProfileHeaderSkeleton";
 
 const ProfilePage = () => {
   const { username } = useParams();
@@ -29,66 +29,60 @@ const ProfilePage = () => {
     toggleFollow({ variables: { username } });
   };
 
-  if (loading)
-    return (
-      <div className="flex justify-center pt-20">
-        <Loader2 className="animate-spin" />
-      </div>
-    );
-
   return (
     <Profile>
-      <div className="flex flex-col gap-6">
-        {/* Header Section */}
-        <header className="flex flex-row items-center gap-8 md:gap-12">
-          <Profile.Avatar
-            avatarUrl={data?.getProfile.avatarUrl || "/ig-default.jpg"}
-          />
-          <section className="flex flex-col gap-2">
-            <div className="flex flex-wrap items-center gap-4">
-              <Profile.Username name={data?.getProfile.username || ""} />
-              <Profile.SettingsButton onClick={() => setIsSettingsOpen(true)} />
+      {loading ? (
+        <ProfileHeaderSkeleton />
+      ) : (
+        <div className="flex flex-col gap-6">
+          {/* Header Section */}
+          <header className="flex flex-row items-center gap-8 md:gap-12">
+            <Profile.Avatar
+              avatarUrl={data?.getProfile.avatarUrl || "/ig-default.jpg"}
+            />
+            <section className="flex flex-col gap-2">
+              <div className="flex flex-wrap items-center gap-4">
+                <Profile.Username name={data?.getProfile.username || ""} />
+                <Profile.SettingsButton
+                  onClick={() => setIsSettingsOpen(true)}
+                />
+              </div>
+              <Profile.DisplayName name={data?.getProfile.displayName || ""} />
+              <Profile.Stats
+                postsCount={data?.getProfile.postsCount || 0}
+                followersCount={data?.getProfile.followersCount || 0}
+                followingCount={data?.getProfile.followingCount || 0}
+                username={data?.getProfile.username || ""}
+              />
+            </section>
+          </header>
+
+          <Profile.Bio text={data?.getProfile.bio || ""} />
+
+          {data?.getProfile.isMe ? (
+            <div className="flex gap-4">
+              <Profile.ActionButton
+                label="Edit profile"
+                onClick={handleEditProfile}
+              />
+              <Profile.ActionButton label="View archive" onClick={() => {}} />
             </div>
-            <Profile.DisplayName name={data?.getProfile.displayName || ""} />
-            <Profile.Stats
-              postsCount={data?.getProfile.postsCount || 0}
-              followersCount={data?.getProfile.followersCount || 0}
-              followingCount={data?.getProfile.followingCount || 0}
-              username={data?.getProfile.username || ""}
-            />
-          </section>
-        </header>
-
-        <Profile.Bio text={data?.getProfile.bio || ""} />
-
-        {data?.getProfile.isMe ? (
-          <div className="flex gap-4">
-            <Profile.ActionButton
-              label="Edit profile"
-              onClick={handleEditProfile}
-            />
-            <Profile.ActionButton label="View archive" onClick={() => {}} />
-          </div>
-        ) : (
-          <div className="flex gap-4">
-            <Profile.ActionButton
-              label={data?.getProfile.isFollowing ? "Following" : "Follow"}
-              onClick={handleToggleFollow}
-              className={
-                !data?.getProfile.isFollowing
-                  ? "bg-indigo-800 hover:bg-indigo-700"
-                  : ""
-              }
-            />
-            <Profile.ActionButton label="Message" onClick={() => {}} />
-          </div>
-        )}
-
-        {/* --- STORY HIGHLIGHTS (Optional placeholder) --- */}
-        <div className="flex gap-4 overflow-x-auto pb-10 scrollbar-hide">
-          {/* Repeat Story circles here if needed */}
+          ) : (
+            <div className="flex gap-4">
+              <Profile.ActionButton
+                label={data?.getProfile.isFollowing ? "Following" : "Follow"}
+                onClick={handleToggleFollow}
+                className={
+                  !data?.getProfile.isFollowing
+                    ? "bg-indigo-800 hover:bg-indigo-700"
+                    : ""
+                }
+              />
+              <Profile.ActionButton label="Message" onClick={() => {}} />
+            </div>
+          )}
         </div>
-      </div>
+      )}
 
       <Profile.Content profileId={data?.getProfile.id || ""} />
 
