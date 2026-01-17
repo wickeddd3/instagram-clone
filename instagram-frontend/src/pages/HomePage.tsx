520a27ea4f5315b2d5109bb6c3f1313f9c000abf
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-17 16:47:50 +0800

    feat: home page posts infinite scroll

--- instagram-frontend/src/pages/HomePage.tsx
+++ instagram-frontend/src/pages/HomePage.tsx
@@ -1,14 +1,51 @@
 import { Stories } from "../components/Stories";
 import { Post } from "../components/posts/Post";
-import { GET_FEED } from "../graphql/queries/post";
+import { GET_FEED_POSTS } from "../graphql/queries/post";
 import { useQuery } from "@apollo/client/react";
-import type { FeedData } from "../types/post";
+import type { FeedPostsdData } from "../types/post";
 import { AuthUser } from "../components/AuthUser";
 import { SuggestionsSidebar } from "../components/layouts/SuggestionsSidebar";
 import { PostSkeleton } from "../components/loaders/PostSkeleton";
+import { useEffect, useRef } from "react";
+import { Loader } from "lucide-react";
 
 const HomePage = () => {
-  const { loading, error, data } = useQuery<FeedData>(GET_FEED);
+  const { data, loading, error, fetchMore } = useQuery<FeedPostsdData>(
+    GET_FEED_POSTS,
+    {
+      variables: { cursor: null, limit: 5 },
+    }
+  );
+
+  const {
+    posts = [],
+    hasMore = false,
+    nextCursor = null,
+  } = data?.getFeedPosts || {};
+
+  const sentinelRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Only fetch more if there is more to fetch
+    if (!sentinelRef.current || loading || !hasMore) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log(entries[0].isIntersecting);
+        if (entries[0].isIntersecting) {
+          console.log("intersect");
+          fetchMore({
+            variables: { cursor: nextCursor },
+          });
+        }
+      },
+      { threshold: 0.8 }
+    );
+
+    observer.observe(sentinelRef.current);
+
+    return () => observer.disconnect();
+  }, [nextCursor, hasMore, loading, fetchMore]);
 
   if (error) {
     return (
@@ -24,18 +61,37 @@ const HomePage = () => {
       <div className="w-full lg:w-[630px] flex flex-col gap-4 pt-4">
         <Stories />
 
-        {loading ? (
+        {loading && !posts.length && (
           <div className="w-full flex flex-col gap-8">
             {[...Array(8)].map((_, i) => (
               <PostSkeleton key={i} />
             ))}
           </div>
-        ) : (
+        )}
+
+        {!!posts.length && (
           <div className="flex flex-col gap-6">
-            {data?.getFeed.map((post) => (
+            {posts.map((post) => (
               <Post key={post.id} post={post} />
             ))}
           </div>
+        )}
+
+        {/* The Sentinel / Loading Indicator */}
+        {/* Only show the sentinel/loader if there's more content expected */}
+        {hasMore && (
+          <div
+            ref={sentinelRef}
+            className="w-full flex justify-center items-center py-10 pb-5"
+          >
+            <Loader className="animate-spin text-gray-500" size={34} />
+          </div>
+        )}
+
+        {!hasMore && posts.length > 0 && (
+          <p className="text-gray-500 text-xs text-center py-10">
+            You've caught up with everything!
+          </p>
         )}
       </div>
 
