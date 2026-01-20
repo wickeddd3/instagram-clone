83a4869f7469f23ab8570c0f5f06d096992e1124
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-21 01:55:11 +0800

    feat: use comment fragment

--- instagram-frontend/src/graphql/mutations/comment.ts
+++ instagram-frontend/src/graphql/mutations/comment.ts
@@ -1,18 +1,14 @@
 import { gql } from "@apollo/client";
+import { COMMENT_FIELDS } from "../fragments/comment";
 
 export const ADD_COMMENT = gql`
   mutation AddComment($postId: ID!, $text: String!, $parentId: ID) {
     addComment(postId: $postId, text: $text, parentId: $parentId) {
-      id
-      text
-      createdAt
-      author {
-        id
-        username
-        avatarUrl
-      }
+      ...CommentFields
     }
   }
+
+  ${COMMENT_FIELDS}
 `;
 
 export const TOGGLE_COMMENT_LIKE = gql`
