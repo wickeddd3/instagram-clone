52e2f60510e92194592e781bd1aff687bd74f434
wickeddd3 <pablophilipandrew@gmail.com>
2026-01-22 03:21:32 +0800

    feat: wrap FollowItem in link to redirect to profile

--- instagram-frontend/src/components/follow/FollowItem.tsx
+++ instagram-frontend/src/components/follow/FollowItem.tsx
@@ -4,6 +4,7 @@ import {
   REMOVE_FOLLOWER,
   REMOVE_FOLLOWING,
 } from "../../graphql/mutations/profile";
+import { Link } from "react-router-dom";
 
 interface FollowItemProps {
   profile: ProfileData;
@@ -22,30 +23,35 @@ export const FollowItem = ({ profile, type }: FollowItemProps) => {
   };
 
   return (
-    <li className="flex justify-between items-center">
-      <div className="flex items-center gap-2">
-        <div className="w-12 h-12 rounded-full p-0.5">
-          <div className="bg-[#0d1015] p-0.5 rounded-full w-full h-full">
-            <img
-              src={profile.avatarUrl || "/ig-default.jpg"}
-              alt={`${profile.username}-avatar`}
-              className="rounded-full w-full h-full object-cover"
-            />
+    <li className="w-full">
+      <Link
+        to={`/${profile.username}`}
+        className="flex justify-between items-center"
+      >
+        <div className="flex items-center gap-2">
+          <div className="w-12 h-12 rounded-full p-0.5">
+            <div className="bg-[#0d1015] p-0.5 rounded-full w-full h-full">
+              <img
+                src={profile.avatarUrl || "/ig-default.jpg"}
+                alt={`${profile.username}-avatar`}
+                className="rounded-full w-full h-full object-cover"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <span className="font-semibold text-sm">{profile.username}</span>
+            <h1 className="text-sm font-normal text-neutral-400">
+              {profile.displayName}
+            </h1>
           </div>
         </div>
-        <div className="flex flex-col">
-          <span className="font-semibold text-sm">{profile.username}</span>
-          <h1 className="text-sm font-normal text-neutral-400">
-            {profile.displayName}
-          </h1>
-        </div>
-      </div>
-      <button
-        onClick={handleToggleFollow}
-        className="bg-neutral-800 px-4 py-1 rounded-lg text-sm cursor-pointer"
-      >
-        {type === "follower" ? "Remove" : "Following"}
-      </button>
+        <button
+          onClick={handleToggleFollow}
+          className="bg-neutral-800 px-4 py-1 rounded-lg text-sm cursor-pointer"
+        >
+          {type === "follower" ? "Remove" : "Following"}
+        </button>
+      </Link>
     </li>
   );
 };
