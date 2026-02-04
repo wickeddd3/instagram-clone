8a53632d4a4c5cd793fab21ef81c3a822f98fd66
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-04 22:26:36 +0800

    feat: handle error and remove dotconfig

--- instagram-backend/src/lib/supabase.ts
+++ instagram-backend/src/lib/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY || "";
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("❌ Supabase Env Missing:", { supabaseUrl, supabaseKey });
+  throw new Error("SUPABASE_URL and SUPABASE_SERVICE_KEY are required.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
