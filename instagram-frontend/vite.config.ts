3540f70e47f2152d04920b150dc2ed7e5edc5158
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-04 22:25:57 +0800

    feat: update config

--- instagram-frontend/vite.config.ts
+++ instagram-frontend/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Listen on all local IPs
+    port: 5173,
+  },
 });
