09c0f59aad13f53c0c873ef6ab2d2857d74e1d18
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-17 23:31:09 +0800

    feat: add hasCloseButton state

--- instagram-frontend/src/contexts/ModalContext.tsx
+++ instagram-frontend/src/contexts/ModalContext.tsx
@@ -3,7 +3,14 @@ import { createContext, useContext, useState, type ReactNode } from "react";
 interface ModalContextType {
   isOpen: boolean;
   content: ReactNode;
-  openModal: (content: ReactNode) => void;
+  hasCloseButton: boolean;
+  openModal: ({
+    content,
+    hasCloseButton,
+  }: {
+    content: ReactNode;
+    hasCloseButton?: boolean;
+  }) => void;
   closeModal: () => void;
 }
 
@@ -11,20 +18,31 @@ export const ModalContext = createContext<ModalContextType | null>(null);
 
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasCloseButton, setHasCloseButton] = useState(false);
   const [content, setContent] = useState<ReactNode | null>(null);
 
-  const openModal = (modalContent: ReactNode) => {
-    setContent(modalContent);
+  const openModal = ({
+    content,
+    hasCloseButton = false,
+  }: {
+    content: ReactNode;
+    hasCloseButton?: boolean;
+  }) => {
+    setContent(content);
+    setHasCloseButton(hasCloseButton);
     setIsOpen(true);
   };
 
   const closeModal = () => {
     setContent(null);
+    setHasCloseButton(false);
     setIsOpen(false);
   };
 
   return (
-    <ModalContext.Provider value={{ isOpen, content, openModal, closeModal }}>
+    <ModalContext.Provider
+      value={{ isOpen, content, hasCloseButton, openModal, closeModal }}
+    >
       {children}
     </ModalContext.Provider>
   );
