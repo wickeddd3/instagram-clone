6e597c0e5a50fa0709264cc7db0d3ca18d780884
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-19 02:20:18 +0800

    cut: remove src/utils

--- instagram-frontend/src/utils/date.ts
+++
@@ -1,6 +0,0 @@
-import { formatDistanceToNow } from "date-fns";
-
-export const formatDateToNow = (dateString: string): string => {
-  if (!dateString) return "";
-  return formatDistanceToNow(new Date(dateString), { addSuffix: true });
-};
