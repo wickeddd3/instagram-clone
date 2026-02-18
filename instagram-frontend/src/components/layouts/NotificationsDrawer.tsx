53852479a844b28eb5d4f319f88db444c3f48f71
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-18 15:08:44 +0800

    feat: replace NotificationsSidebar with NotificationsDrawer

---
+++ instagram-frontend/src/components/layouts/NotificationsDrawer.tsx
@@ -0,0 +1,10 @@
+export const NotificationsDrawer = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-8">Notifications</h2>
+
+      {/* Results List */}
+      <div className="flex-1 overflow-y-auto">list</div>
+    </div>
+  );
+};
