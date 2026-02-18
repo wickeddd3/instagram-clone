39ec58dbb89503e49972a6a5b07835344e1cfff4
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-18 15:06:32 +0800

    feat: wrap main App component in DrawerProvider

--- instagram-frontend/src/main.tsx
+++ instagram-frontend/src/main.tsx
@@ -7,16 +7,19 @@ import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { client } from "./lib/apollo.ts";
 import { PostProvider } from "./contexts/PostContext.tsx";
 import { ModalProvider } from "./contexts/ModalContext.tsx";
+import { DrawerProvider } from "./contexts/DrawerContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <AuthProvider>
-        <ModalProvider>
-          <PostProvider>
-            <App />
-          </PostProvider>
-        </ModalProvider>
+        <DrawerProvider>
+          <ModalProvider>
+            <PostProvider>
+              <App />
+            </PostProvider>
+          </ModalProvider>
+        </DrawerProvider>
       </AuthProvider>
     </ApolloProvider>
   </StrictMode>,
