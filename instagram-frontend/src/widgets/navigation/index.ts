bbed44ab14b0355fb0b65d46490e317c0577c705
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-20 03:54:41 +0800

    refactor: move MobileHeader, MobileNav, and Sidebar to widgets

---
+++ instagram-frontend/src/widgets/navigation/index.ts
@@ -0,0 +1,5 @@
+export { MobileHeader } from "./ui/MobileHeader";
+export { MobileNav } from "./ui/MobileNav";
+export { Sidebar } from "./ui/Sidebar";
+export { AuthAppbar } from "./ui/AuthAppbar";
+export { Footer } from "./ui/Footer";
---
+++ instagram-frontend/src/widgets/navigation/ui/MobileHeader.tsx
@@ -0,0 +1,21 @@
+import { Heart } from "lucide-react";
+import { MobileSearch } from "@/components/search/MobileSearch";
+
+export const MobileHeader = () => {
+  return (
+    <header className="w-full px-3 py-2 bg-[#0d1015] flex items-center justify-between">
+      <h1
+        className="text-3xl font-medium"
+        style={{ fontFamily: "Grand Hotel, cursive" }}
+      >
+        Instagram
+      </h1>
+      <div className="flex gap-3">
+        <MobileSearch />
+        <button aria-label="Menu" className="text-white cursor-pointer">
+          <Heart size={24} />
+        </button>
+      </div>
+    </header>
+  );
+};
