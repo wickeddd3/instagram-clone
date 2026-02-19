ef1eb28b75f93f2743a17009f7014c15d4959107
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-19 19:54:10 +0800

    feat: re-export post entity ui

--- instagram-frontend/src/entities/post/index.ts
+++ instagram-frontend/src/entities/post/index.ts
@@ -1,3 +1,9 @@
+export { PostCard } from "./ui/PostCard";
 export { PostHeader } from "./ui/PostHeader";
 export { PostImage } from "./ui/PostImage";
+export { PostDetails } from "./ui/PostDetails";
+export { PostSkeleton } from "./ui/PostSkeleton";
+export { CommentButton } from "./ui/CommentButton";
+export { ChatButton } from "./ui/ChatButton";
 export type { Post } from "./model/types";
+export { POST_FRAGMENT } from "./api/fragments";
