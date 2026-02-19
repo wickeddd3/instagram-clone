bfbd2ce5a6663930b1f4dda44b823b5b2c2a296c
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-20 03:59:33 +0800

    feat: use post modal widget and handle opening of modal

--- instagram-frontend/src/widgets/feed/ui/FeedCard.tsx
+++ instagram-frontend/src/widgets/feed/ui/FeedCard.tsx
@@ -4,18 +4,28 @@ import {
   ChatButton,
   CommentButton,
   PostCard,
+  TotalComments,
   type Post,
 } from "@/entities/post";
 import { LikeButton } from "@/features/post/like-post";
 import { SaveButton } from "@/features/post/save-post";
-import { TotalComments } from "@/features/post/view-post-modal";
 import { AddCommentField } from "@/features/comment/add-comment";
+import { usePost } from "@/app/providers/PostContext";
+import { usePostModal } from "@/widgets/post-modal";
 
 export const FeedCard = ({ post }: { post: Post }) => {
+  const { selectPost } = usePost();
+  const { openPostDetailsModal } = usePostModal();
+
   // Ref for focusing the comment input when comment button is clicked
   const commentInputRef = useRef<HTMLInputElement>(null);
   const handleFocusComment = () => {
     commentInputRef.current?.focus();
+  };
+
+  const handleOpenPostDetailsModal = (post: Post) => {
+    selectPost(post);
+    openPostDetailsModal();
   };
 
   return (
@@ -27,7 +37,12 @@ export const FeedCard = ({ post }: { post: Post }) => {
       saveButtonSlot={<SaveButton post={post} />}
       commentButtonSlot={<CommentButton onClick={handleFocusComment} />}
       chatButtonSlot={<ChatButton />}
-      totalCommentsSlot={<TotalComments post={post} />}
+      totalCommentsSlot={
+        <TotalComments
+          commentsCount={post.commentsCount}
+          onClick={() => handleOpenPostDetailsModal(post)}
+        />
+      }
       commentFieldSlot={
         <AddCommentField postId={post.id} inputRef={commentInputRef} />
       }
