72797a66a2095ffcd368bf4e9bc86a1e3e810729
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-23 04:46:57 +0800

    feat: add explore-feed widget

---
+++ instagram-frontend/src/widgets/explore-feed/model/useInfiniteExploreFeed.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@apollo/client/react";
+import { GET_EXPLORE_POSTS } from "../api/query";
+import type { ExplorePosts } from "./types";
+import { NetworkStatus } from "@apollo/client";
+
+export const useInfiniteExploreFeed = () => {
+  const { data, loading, error, fetchMore, networkStatus } =
+    useQuery<ExplorePosts>(GET_EXPLORE_POSTS, {
+      variables: { cursor: null, limit: 9 },
+      notifyOnNetworkStatusChange: true, // Important for loading state during fetchMore
+    });
+
+  const {
+    posts = [],
+    hasMore = false,
+    nextCursor = null,
+  } = data?.getExplorePosts || {};
+
+  const loadMore = () => {
+    if (!hasMore || loading) return;
+    fetchMore({
+      variables: { cursor: nextCursor },
+    });
+  };
+
+  return {
+    posts,
+    hasMore,
+    nextCursor,
+    loading,
+    error,
+    isLoadingMore: networkStatus === NetworkStatus.refetch, // Indicates loading state during fetchMore
+    loadMore,
+  };
+};
