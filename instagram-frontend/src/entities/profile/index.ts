c2cb4ff74b65c346e5d37375906a9b3cfa049821
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-22 19:58:33 +0800

    feat: add profile entity ProfileLink

--- instagram-frontend/src/entities/profile/index.ts
+++ instagram-frontend/src/entities/profile/index.ts
@@ -2,4 +2,5 @@ export { ProfileHeaderSkeleton } from "./ui/ProfileHeaderSkeleton";
 export { ProfilePostsSkeleton } from "./ui/ProfilePostsSkeleton";
 export { ProfileHeader } from "./ui/ProfileHeader";
 export { ProfileContent } from "./ui/ProfileContent";
+export { ProfileLink } from "./ui/ProfileLink";
 export type { Profile } from "./model/types";
