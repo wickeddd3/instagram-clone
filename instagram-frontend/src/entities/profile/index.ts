f67be8f6305a6c3a5bbf970daaebdfa272a77a55
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-22 17:46:05 +0800

    feat: add profile entity ui

--- instagram-frontend/src/entities/profile/index.ts
+++ instagram-frontend/src/entities/profile/index.ts
@@ -1,1 +1,5 @@
+export { ProfileHeaderSkeleton } from "./ui/ProfileHeaderSkeleton";
+export { ProfilePostsSkeleton } from "./ui/ProfilePostsSkeleton";
+export { ProfileHeader } from "./ui/ProfileHeader";
+export { ProfileContent } from "./ui/ProfileContent";
 export type { Profile } from "./model/types";
