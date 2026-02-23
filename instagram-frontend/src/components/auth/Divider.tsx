89d368a13c948d54dec82b03b3ef8b84fe7d73bf
wickeddd3 <pablophilipandrew@gmail.com>
2026-02-23 17:32:52 +0800

    cut: remove auth components

--- instagram-frontend/src/components/auth/Divider.tsx
+++
@@ -1,9 +0,0 @@
-export const Divider = () => {
-  return (
-    <div className="w-full flex justify-center items-center gap-4">
-      <div className="flex-1 border-t border-gray-800"></div>
-      <span className="w-fit text-sm font-bold text-gray-400">OR</span>
-      <div className="flex-1 border-t border-gray-800"></div>
-    </div>
-  );
-};
--- instagram-frontend/src/components/auth/links/ForgotPasswordLinkt.tsx
+++
@@ -1,7 +0,0 @@
-export const ForgotPasswordLink = () => {
-  return (
-    <span className="text-sm font-semibold text-gray-200 cursor-pointer">
-      Forgot Password?
-    </span>
-  );
-};
