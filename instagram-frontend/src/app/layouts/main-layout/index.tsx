05ce2338a5172865848a192adc1b55704426195a
wickeddd3 <pablophilipandrew@gmail.com>
2026-03-05 18:09:24 +0800

    feat: move scrollbar back to body

--- instagram-frontend/src/app/layouts/main-layout/index.tsx
+++ instagram-frontend/src/app/layouts/main-layout/index.tsx
@@ -8,7 +8,7 @@ import { LayoutDrawer } from "./LayoutDrawer";
 
 export const MainLayout = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row h-screen overflow-y-auto custom-scrollbar bg-[#0d1015] text-white">
+    <div className="w-full flex flex-col md:flex-row bg-[#0d1015] text-white">
       {/* Top Header - Hidden on Desktop */}
       <div className="md:hidden fixed top-0 w-full z-50 bg-[#0d1015]">
         <MobileHeader />
