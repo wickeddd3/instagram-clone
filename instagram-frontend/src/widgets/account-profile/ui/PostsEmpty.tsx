59b4c1d15e908249ba445658e77ad5ae47d854ea
wickeddd3 <pablophilipandrew@gmail.com>
2026-03-08 04:31:49 +0800

    feat: add text-center class

--- instagram-frontend/src/widgets/account-profile/ui/PostsEmpty.tsx
+++ instagram-frontend/src/widgets/account-profile/ui/PostsEmpty.tsx
@@ -13,7 +13,7 @@ export const PostsEmpty = ({
     <div className="flex flex-col justify-center items-center gap-4 p-12">
       <span className="border border-gray-50 rounded-full p-3">{icon}</span>
       <h1 className="text-2xl font-bold">{title}</h1>
-      <p className="font-light">{subtitle}</p>
+      <p className="font-light text-center">{subtitle}</p>
     </div>
   );
 };
