b5e7772d92fce89da7541f9babb22f1c34291f26
wickeddd3 <pablophilipandrew@gmail.com>
2026-03-08 00:13:12 +0800

    feat: add created post and saved post to cache profilePosts and savedPosts

--- instagram-frontend/src/features/post/save-post/model/useSavePost.ts
+++ instagram-frontend/src/features/post/save-post/model/useSavePost.ts
@@ -1,8 +1,12 @@
 import { useMutation } from "@apollo/client/react";
 import { TOGGLE_POST_SAVE } from "../api/mutation";
 import type { Post } from "@/entities/post";
+import { useAuth } from "@/app/providers/AuthContext";
 
 export const useSavePost = ({ post }: { post: Post }) => {
+  const { authProfile } = useAuth();
+  const profileId = authProfile?.id || "";
+
   const [togglePostSave] = useMutation(TOGGLE_POST_SAVE, {
     optimisticResponse: (vars) => ({
       togglePostSave: {
@@ -12,10 +16,42 @@ export const useSavePost = ({ post }: { post: Post }) => {
       },
     }),
     update(cache, { data: { togglePostSave } }: any) {
+      const isSaved = togglePostSave.isSaved;
+
       cache.modify({
         id: cache.identify({ __typename: "Post", id: post.id }),
         fields: {
-          isSaved: () => togglePostSave.isSaved,
+          isSaved: () => isSaved,
+        },
+      });
+
+      cache.modify({
+        fields: {
+          getSavedPosts(existing, { storeFieldName }) {
+            if (!storeFieldName.includes(profileId)) return existing;
+
+            // Identify post cache reference ID
+            const postRef = cache.identify({
+              __typename: "Post",
+              id: post.id,
+            });
+
+            if (isSaved) {
+              return {
+                ...existing,
+                posts: [{ __ref: postRef }, ...existing.posts],
+              };
+            } else {
+              // Remove post from existing saved posts
+              const filteredPosts = existing.posts.filter(
+                (post: { __ref: string }) => post.__ref !== postRef,
+              );
+              return {
+                ...existing,
+                posts: filteredPosts,
+              };
+            }
+          },
         },
       });
     },
