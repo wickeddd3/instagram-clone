9bf456effcdc9940fc54f1e9f2d79f8a451de62c
wickeddd3 <pablophilipandrew@gmail.com>
2026-03-15 18:48:52 +0800

    feat: add inbox page coming soon placeholder

--- instagram-frontend/src/pages/InboxPage.tsx
+++ instagram-frontend/src/pages/InboxPage.tsx
@@ -1,5 +1,15 @@
+import { MessageCircle } from "lucide-react";
+
 const InboxPage = () => {
-  return <h1 className="text-center">inbox page</h1>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <MessageCircle size={60} />
+        <h2 className="text-2xl font-bold">CHAT MESSAGE</h2>
+        <p className="text-lg font-light">Coming soon</p>
+      </div>
+    </div>
+  );
 };
 
 export default InboxPage;
