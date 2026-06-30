56732de3ec47469a4c0a4aa4581d7af6bf9f9b72
wickeddd3 <pablophilipandrew@gmail.com>
2026-07-01 03:56:20 +0800

    feat: disable forgot password link

--- instagram-frontend/src/widgets/auth/ui/ForgotPasswordLinkt.tsx
+++ instagram-frontend/src/widgets/auth/ui/ForgotPasswordLinkt.tsx
@@ -1,6 +1,9 @@
 export const ForgotPasswordLink = () => {
   return (
-    <span className="text-sm font-semibold text-gray-200 cursor-pointer">
+    <span
+      className="text-sm font-semibold text-gray-500 cursor-no-drop"
+      aria-disabled
+    >
       Forgot Password?
     </span>
   );
